a295b4316629c6ece53e5e00f795af19f85bc4d1
Victor Santiago <victorhsantiago3@gmail.com>
2022-04-10 19:50:09 -0300

    feat: add create course mutation

--- classroom/src/services/courses.services.ts
+++ classroom/src/services/courses.services.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
 import { PrismaService } from 'src/database/prisma/prisma.service';
+
+interface CreateCourseParams {
+  title: string;
+}
 
 @Injectable()
 export class CoursesService {
@@ -11,5 +17,19 @@ export class CoursesService {
 
   getCourseById(id: string) {
     return this.prisma.course.findUnique({ where: { id } });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, { lower: true });
+
+    const courseAlreadyExists = await this.prisma.course.findUnique({
+      where: { slug },
+    });
+
+    if (courseAlreadyExists) throw new Error('Course already exists.');
+
+    return this.prisma.course.create({
+      data: { title, slug },
+    });
   }
 }
