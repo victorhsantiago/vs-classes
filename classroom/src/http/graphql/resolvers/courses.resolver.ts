da92e7364459020c9a9fd78315fcde60236c29fe
Victor Santiago <victorhsantiago3@gmail.com>
2022-04-10 20:27:23 -0300

    feat: add course listings

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,19 +1,43 @@
-import { UseGuards } from '@nestjs/common';
+import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 import { CoursesService } from 'src/services/courses.services';
+import { EnrollmentsService } from 'src/services/enrollments.services';
+import { StudentsService } from 'src/services/students.services';
 import { CreateCourseInput } from '../inputs/create-course-input';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
 export class CoursesResolver {
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private enrollmentsService: EnrollmentsService,
+    private studentsService: StudentsService,
+  ) {}
 
   @Query(() => [Course])
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
+  }
+
+  @Query(() => Course)
+  @UseGuards(AuthorizationGuard)
+  async course(@Args('id') id: string, @CurrentUser() user: AuthUser) {
+    const student = await this.studentsService.getStudentByAuthUserId(user.sub);
+
+    if (!student) throw new Error('Student not found.');
+
+    const enrollment = await this.enrollmentsService.getByCourseAndStudentId({
+      courseId: id,
+      studentId: student.id,
+    });
+
+    if (!enrollment) throw new UnauthorizedException();
+
+    return this.coursesService.getCourseById(id);
   }
 
   @Mutation(() => Course)
